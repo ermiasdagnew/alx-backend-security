3c6cad5331125258f131fae0cd5b275c4e6f6d63
Ermias Aweke <ermiasdagnewa@gmail.com>
2026-01-17 20:06:15 +0300

    Update wsgi.py

--- alx_backend_security/wsgi.py
+++ alx_backend_security/wsgi.py
@@ -1,12 +1,7 @@
-"""
-WSGI config for alx_backend_security project.
-
-It exposes the WSGI callable as a module-level variable named `application`.
-"""
-
 import os
 from django.core.wsgi import get_wsgi_application
 
+# Make sure this matches your package folder name
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alx_backend_security.settings")
 
 application = get_wsgi_application()
