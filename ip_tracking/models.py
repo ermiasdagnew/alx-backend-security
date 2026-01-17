ef3d3c8cc80ef05081c155c151af8c59005f5fb9
Ermias Aweke <ermiasdagnewa@gmail.com>
2026-01-17 18:24:02 +0300

    Update models.py

--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -1,9 +1,8 @@
-from django.db import models
-
-class RequestLog(models.Model):
+# ip_tracking/models.py
+class SuspiciousIP(models.Model):
     ip_address = models.GenericIPAddressField()
-    path = models.CharField(max_length=255)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    reason = models.CharField(max_length=255)
+    flagged_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.ip_address} - {self.path}"
+        return f"{self.ip_address} - {self.reason}"
