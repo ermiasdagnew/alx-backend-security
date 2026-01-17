c6a00873207ba8c63b44b89a8ccf0b7ce08b5875
Ermias Aweke <ermiasdagnewa@gmail.com>
2026-01-17 20:06:48 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
-import os, sys
-def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alx_backend_security.settings')
-    from django.core.management import execute_from_command_line
+import os
+import sys
+
+if __name__ == "__main__":
+    # Same here: use underscore package name
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alx_backend_security.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed?"
+        ) from exc
     execute_from_command_line(sys.argv)
-if __name__ == '__main__':
-    main()
